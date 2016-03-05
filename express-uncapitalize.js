61cd02b32e6c48390b7e2770200cc451eb9672cc
vanielf <vanielf@gmail.com>
2016-03-05 12:56:49 -0300

    Update express-uncapitalize.js

--- express-uncapitalize.js
+++ express-uncapitalize.js
@@ -2,12 +2,26 @@
 // Copyright (c) 2012 Jamie Steven, jamiesteven on github.
 // Licensed under MIT
 
-module.exports = function() {
-    return function(req, res, next) {
-        if (/[A-Z]/.test(req.url)) {
-            res.redirect(301, req.url.toLowerCase());
-        } else {
-            next();
-        }
+module.exports = function(ignore){
+    if(ignore){
+        return function(req, res, next){
+            if(ignore(req)){
+                next();
+            }else{
+                if(/[A-Z]/.test(req.url)){
+                    res.redirect(301, req.url.toLowerCase());
+                }else{
+                    next();
+                }
+            }
+        };
+    }else{
+        return function(req, res, next){
+            if(/[A-Z]/.test(req.url)){
+                res.redirect(301, req.url.toLowerCase());
+            }else{
+                next();
+            }
+        };
     }
 };
